040deed7ec225aec21acb0abd1fd415dad859e56
vidal <tanguy51.tv@gmail.com>
2023-04-05 15:21:52 +0200

    ajout début constants

---
+++ constants_IKI.py
@@ -0,0 +1,18 @@
+LEXEM_REGEXES = [
+
+    (r'\bvar\b', "VAR"),
+    (r'\bint\b', "TYPE_INT"),
+    (r'\bbool\b', "TYPE_BOOL"),
+    (r'\bread\b', "READ"),
+    (r'\bwrite\b', "WRITE"),
+    (r'\bwhile\b', "WHILE"),
+    (r'\bloop\b', "LOOP"),
+    (r'\bend\b', "ENDW"),
+    (r'\btrue\b', "TRUE"),
+    (r'\bfalse\b', "FALSE"),
+    (r'\bor\b', "OR"),
+    (r'\band\b', "AND"),
+    (r'\bnot\b', "NOT")
+
+]
+
